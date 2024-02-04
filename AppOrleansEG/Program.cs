d7ef196f5bbbfeef14b5491ef320adc865c7cc7c
Eddie Girón <eddiegiron188@gmail.com>
2024-02-03 19:02:11 -0600

    Commit

---
+++ AppOrleansEG/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Orleans;
+using Orleans.Hosting;
+
+IHostBuilder builder = Host.CreateDefaultBuilder(args)
+    .UseOrleans(silo =>
+    {
+        silo.UseLocalhostClustering()
+            .ConfigureLogging(logging => logging.AddConsole());
+    })
+    .UseConsoleLifetime();
+
+using IHost host = builder.Build();
+
+await host.RunAsync();
